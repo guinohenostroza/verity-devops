51aa929dd58ed2c6f6f7bbc052db484606523a3d
guinohenostroza <guinohenostroza@gmail.com>
2020-11-12 19:40:22 -0500

    Update ExpenseApplicationTest.java

--- src/test/java/com/expense/ExpenseApplicationTest.java
+++ src/test/java/com/expense/ExpenseApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExpenseApplicationTest {
-        // Comment Test
+        // Comment Test 002
 	@Test
 	public void contextLoads() {
 	}
